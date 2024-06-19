5b717b666fd95cb3e1755747b0e1d651686b2060
Metyme1 <millionmety@gmail.com>
2024-06-19 18:38:45 +0300

    room page edited

--- src/Rooms.js
+++ src/Rooms.js
@@ -5,6 +5,10 @@ import room1 from './assets/room1.jpg'; // adjust the path as necessary
 import room2 from './assets/room2.jpg';
 import room3 from './assets/r3.jpg';
 import room4 from './assets/room1.jpg';
+import 'aos/dist/aos.css';
+import AOS from 'aos';
+
+AOS.init();
 
 const rooms = [
   {
@@ -47,38 +51,29 @@ const Rooms = () => {
       </header>
       <section className="py-16">
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          {rooms.map((room) => (
-            <div key={room.id} className="bg-white shadow-md rounded-lg overflow-hidden mb-8">
-              <div className="grid grid-cols-1 md:grid-cols-2">
-                <img src={room.image} alt={room.name} className="w-full h-64 object-cover"/>
-                <div className="p-4 flex flex-col justify-between">
-                  <div>
-                    <h3 className="text-2xl font-bold mb-2">{room.name}</h3>
-                    <p className="text-gray-600 mb-4">{room.description}</p>
-                  </div>
-                  <div>
-                    <p className="text-xl font-bold mb-2">Price {room.price}</p>
-                    <Link to={`/rooms/${room.id}`} className="bg-custom-blue text-white px-4 py-2 rounded-md">View Detail</Link>
-                  </div>
+          {rooms.map((room, index) => (
+            <div 
+              key={room.id} 
+              className={`bg-white shadow-md rounded-lg overflow-hidden mb-8 flex ${index % 2 === 0 ? '' : 'flex-row-reverse'}`} 
+              data-aos={`${index % 2 === 0 ? 'fade-right' : 'fade-left'}`}
+              style={{ height: '400px' }} // Adjust the height here
+            >
+              <img src={room.image} alt={room.name} className="w-1/2 h-full object-cover"/>
+              <div className="p-4 flex flex-col justify-between w-1/2">
+                <div>
+                  <h3 className="text-2xl font-bold mb-2">{room.name}</h3>
+                  <p className="text-gray-600 mb-4">{room.description}</p>
+                </div>
+                <div>
+                  <p className="text-xl font-bold mb-2">Price {room.price}</p>
+                  <Link to={`/rooms/${room.id}`} className="bg-custom-blue text-white px-4 py-2 rounded-md">View Detail</Link>
                 </div>
               </div>
             </div>
           ))}
         </div>
       </section>
-      <footer className="bg-custom-blue text-white py-4">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-          <div className="flex justify-between">
-            <div>
-              <h1 className="text-lg font-bold">Carivan Dire Hotel</h1>
-            </div>
-            <div>
-              <p>Contact Us</p>
-              <p>+251 0909090909</p>
-            </div>
-          </div>
-        </div>
-      </footer>
+      
     </div>
   );
 }
