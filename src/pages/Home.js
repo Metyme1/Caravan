9e96e6b3c95c0eee6fb5a06aa6c32ba827f8075c
Metyme1 <millionmety@gmail.com>
2024-07-14 05:41:24 +0300

    git home paged edited

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -258,7 +258,7 @@ Relax and unwind in our well-appointed rooms, equipped with modern amenities inc
         </div>
       </div>
     </section>
-    <section className="py-16 bg-white">
+<section className="py-16 bg-white">
   <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <h2 className="text-4xl font-times text-gray-900 text-center mb-8">Meetings and Events</h2>
     <div className="mt-8 bg-white shadow-md rounded-lg overflow-hidden flex flex-col-reverse md:flex-row items-center">
